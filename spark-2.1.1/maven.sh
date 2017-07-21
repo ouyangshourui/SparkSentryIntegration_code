cf6c50bfa7ca5add36e53b77794cd306417dc234
root <root@cdndc-213128087.wdds.com>
2017-07-21 11:26:54 +0800

    change maven.sh pom.xml  dev/make-distribution.sh

--- spark-2.1.1/maven.sh
+++ spark-2.1.1/maven.sh
@@ -1,3 +1,4 @@
 export MAVEN_OPTS="-Xmx2g -XX:MaxPermSize=512M -XX:ReservedCodeCacheSize=512m "
 sh dev/make-distribution.sh  \
-   -DskipTests   -Dhadoop.version=2.6.0-cdh5.7.1  -Phadoop-2.6 -Pyarn -Phive  -rf :spark-hive_2.11
+   -DskipTests   -Dhadoop.version=2.6.0-cdh5.7.1  -Phadoop-2.6 -Pyarn -Phive  -Phive-thriftserver 
+   #-DskipTests   -Dhadoop.version=2.6.0-cdh5.7.1  -Phadoop-2.6 -Pyarn -Phive  -Phive-thriftserver -rf :spark-hive_2.11
